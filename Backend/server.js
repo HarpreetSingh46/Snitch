d0182e1fd7601dc96bbc8437927bc3c4ab01c847
Harpreet Singh <theharpreetsingh524@gmail.com>
2026-06-06 23:12:28 +0530

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -1,15 +1,11 @@
 import dotenv from "dotenv";
 import app from "./src/app.js";
 import connectDB from "./src/config/db.js";
-
 dotenv.config();
-
 const PORT = process.env.PORT || 3000;
-
 const startServer = async () => {
     try {
         await connectDB();
-
         app.listen(PORT, () => {
             console.log(`Server listening on port ${PORT}`);
         });
